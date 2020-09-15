d7e6ebe1b128aea9aebd75cd3f40811f1c6c1919
Inxudianz <william.inxcorp@gmail.com>
2020-09-15 14:33:56 +0700

    add boilet route for orderRoute

--- route/mainRoute.js
+++ route/mainRoute.js
@@ -6,6 +6,7 @@ module.exports = function(app) {
 	app.use('/auth', require('./authRoute'));
 	app.use('/profile', require('./profileRoute'));
 	app.use('/home', require('./homeRoute'));
+	app.use('/order', require('./orderRoute'));
 
 	app.use('*', function(request, response) {
 		response.send('<h1>404</h1>');
--- route/orderRoute.js
+++ route/orderRoute.js
@@ -1,4 +1,26 @@
 var router = require('express').Router();
 var firebase = require('./firebase');
+var bodyParser = require('body-parser');
+const { route } = require('./profileRoute');
 
 var database = firebase.firestore();
+router.use(bodyParser.json());
+router.use(bodyParser.urlencoded({ extended: true }));
+
+const orderVersion = 'v1';
+
+route.get(`/${orderVersion}/history/:id`, (request, response) => {
+	var id = request.params.id;
+});
+
+route.post(`/${orderVersion}/history/:id`, (request, response) => {
+	var id = request.params.id;
+});
+
+route.get(`/${orderVersion}/ongoing/:id`, (request, response) => {
+	var id = request.params.id;
+});
+
+route.post(`/${orderVersion}/ongoing/:id`, (request, response) => {
+	var id = request.params.id;
+});
